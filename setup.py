fd35e0251e74150b562c0fc43391120905be4ff0
VV <66221043+vvaneecloo@users.noreply.github.com>
2026-06-17 10:32:33 +0200

    bump: version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbt-addons",
-    version="0.1.0",
+    version="0.0.2",
     packages=find_packages(),
     package_data={
         "addons": [
