0f7f3ce47a88d800f10e72f34c1ad97772e1aa6c
VV <66221043+vvaneecloo@users.noreply.github.com>
2026-06-21 15:32:07 +0200

    feat: pypi description (#11)

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="dbt-addons",
     version="0.0.5",
+    description="Write-Audit-Publish and other missing dbt patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Victor Vaneecloo",
+    author_email="victor.vaneecloo3@gmail.com",
+    url="https://github.com/vvaneecloo/dbt-addons",
+    project_urls={
+        "Documentation": "https://vvaneecloo.github.io/dbt-addons/",
+        "Source": "https://github.com/vvaneecloo/dbt-addons",
+        "Issues": "https://github.com/vvaneecloo/dbt-addons/issues",
+    },
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries",
+    ],
     packages=find_packages(),
     package_data={
         "addons": [
